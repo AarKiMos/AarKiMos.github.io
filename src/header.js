da470d961ef8a5e797db896f2d7d1e616299d02d
Aachman Mittal <m.aachman@gmail.com>
2022-01-11 23:56:13 +0530

    Added: Src dir, index.js, header module and related styles

---
+++ src/header.js
@@ -0,0 +1,9 @@
+function header() {
+    let element = document.createElement('div');
+    element.classList.add('header');
+    element.innerText = 'AarKiMos';
+
+    return element;
+}
+
+export default header;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import header from './header.js'
+
+import './style.css'
+import './headerStyle.css'
+
+let body = document.querySelector('body');
+
+body.appendChild(header());
